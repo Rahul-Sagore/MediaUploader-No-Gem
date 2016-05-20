acb32b823494db7afb3f36aa8e02da0bd51f7566
Rahul-Sagore <rahul.sagore@gmail.com>
2016-05-20 14:03:37 +0530

    Handle duplicate media upload

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -17,23 +17,59 @@ class HomeController < ApplicationController
 
 		 	if fileObj
 		 		# Write the uploaded file
-		 		File.open(Rails.root.join('public', 'images/uploads/', session[:user_id].to_s, fileObj.original_filename), 'w') do |file|
-		      file.write(fileObj.read)
-		    end 
+		 		filename = avoidDuplicates(fileObj.original_filename)
 
-		    # Save the file info into database
-		 		@create_media = Medium.new(params[:new_media])
-        @create_media[:user_id] = session[:user_id]
-        @create_media[:filename] = fileObj.original_filename
-        @create_media[:mime_type] = fileObj.content_type
+		 		if filename
+			 		File.open(Rails.root.join('public', 'images/uploads/', session[:user_id].to_s, filename), 'w') do |file|
+			      file.write(fileObj.read)
+			    end 
 
-        @create_media.save
+			    # Save the file info into database
+			 		@create_media = Medium.new(params[:new_media])
+	        @create_media[:user_id] = session[:user_id]
+	        @create_media[:filename] = filename
+	        @create_media[:mime_type] = fileObj.content_type
+
+	        @create_media.save
+	      else
+	      	flash[:error] = "Duplicate file"
+	      end
 	    end
 	  else
 	  	flash[:error] = "Please select file to upload"
 		end
 
 		redirect_to :action => "index"
+	end
+
+	def avoidDuplicates(filename)
+		fExt = File.extname(filename)
+		fName = File.basename(filename, fExt)
+		isFileExist = Medium.where(:user_id => session[:user_id], :filename => filename)
+
+		if isFileExist.size > 0
+			moreFileExist = Medium.where(["filename LIKE ?", "%#{fName}_copy%"]).where(:user_id => 1)
+			if moreFileExist.size > 0
+				lastEntry = moreFileExist[-1].filename
+			else
+			 	lastEntry = isFileExist[-1].filename
+			end
+			ext = File.extname(lastEntry)
+			fBasename = File.basename(lastEntry, ext)
+			pat = fBasename.match(/(_copy)[0-9]*/)
+
+			if !pat.nil?
+				pat = pat.to_s
+				copy = [pat.slice(0..4), pat.slice(5..-1)]
+				#Slicng for double digit replcement
+				fBasename[-copy[1].size..-1] = (copy[1].to_i + 1).to_s + ext
+				fBasename
+			else
+				fBasename + "_copy1" + ext
+			end
+		else
+			filename
+		end
 	end
 
 end
